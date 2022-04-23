5cc342023c2377bf97f9c0c5cd50de5ff7eb8274
HemanthsaiVarada456 <hemanthsaivarada.vtalent@gmail.com>
2022-04-23 16:01:23 +0530

    my project

---
+++ src/test/java/com/hemanth/springboot/thymeleafdemo/ThymeleafdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hemanth.springboot.thymeleafdemo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
